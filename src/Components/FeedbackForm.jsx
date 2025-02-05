708e2ecb369b510e98995afd48a1c1541118b207
Aman Abuelayyan <153456442+safety-crew@users.noreply.github.com>
2025-02-05 12:27:29 +0200

    Initialize project components

---
+++ src/Components/FeedbackForm.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import "./FeedbackForm.css"; // Import CSS for styling
+
+const FeedbackForm = () => {
+  return (
+    <>
+      <nav>Tell Us What You Think</nav>
+      <form className="feedback-form">
+        <h2>We&apos;d Love to Hear From You!</h2>
+        <p>Please share your feedback with us.</p>
+      </form>
+    </>
+  );
+};
+
+export default FeedbackForm;
